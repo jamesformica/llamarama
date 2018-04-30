56779e03aeef5a76b6fd27eba9fe7da30bc881ff
James Formica <james.formica@gmail.com>
2018-05-01 08:23:21 +1000

    feat: focus start button on menu mount

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
@@ -6,20 +6,32 @@ import { play } from '../../actions'
 
 import styles from './Menu.css'
 
-const Menu = ({ onStart, score }) => (
-  <div className={styles.wrapper}>
-    <div className={styles.menu}>
-      <h1 className={styles.title}>Llamarama</h1>
-      <p>Press &apos;space&apos; or click to jump!</p>
-      <p className={styles.text}>
-        Your score
-        <br />
-        <span className={styles.score}>{score}</span>
-      </p>
-      <button className={styles.start} onClick={onStart}>START</button>
-    </div>
-  </div>
-)
+class Menu extends Component {
+  componentDidMount() {
+    this.button.focus()
+  }
+
+  render() {
+    const { onStart, score } = this.props
+
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles.menu}>
+          <h1 className={styles.title}>Llamarama</h1>
+          <p>Press &apos;space&apos; or click to jump!</p>
+          <p className={styles.text}>
+            Your score
+          <br />
+            <span className={styles.score}>{score}</span>
+          </p>
+          <button className={styles.start} onClick={onStart} ref={(b) => { this.button = b }}>
+            START
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
 
 const mapStateToProps = state => ({
   score: state.score
