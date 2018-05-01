41367ab30dc38806917b8583bf8a994978d86b79
James Formica <james.formica@gmail.com>
2018-05-01 11:16:04 +1000

    feat: add mountain

--- src/helpers/sizeHelper.js
+++ src/helpers/sizeHelper.js
@@ -24,11 +24,11 @@ export const getDesiredTreeHeight = (size, { height }) => {
 export const getDesiredMountainHeight = (size, { height }) => {
   switch (size) {
     case SIZE.small:
-      return height * 0.3
+      return height * 0.2
     case SIZE.medium:
-      return height * 0.5
+      return height * 0.4
     case SIZE.large:
-      return height * 0.8
+      return height * 0.7
     default:
       return 0
   }
--- src/models/Mountain.js
+++ src/models/Mountain.js
@@ -4,8 +4,10 @@ import Drawable from './Drawable'
 import { SIZES } from '../controllers/BackgroundController'
 import { calculateSize, getDesiredMountainHeight } from '../helpers/sizeHelper'
 
+import mountain from '../sprites/mountain.png'
+
 const MOUNTAIN_WIDTH = 300
-const MOUNTAIN_HEIGHT = 200
+const MOUNTAIN_HEIGHT = 234
 
 class Mountain extends Drawable {
   constructor(x, scene) {
@@ -15,14 +17,16 @@ class Mountain extends Drawable {
     const { width, height } = calculateSize(MOUNTAIN_WIDTH, MOUNTAIN_HEIGHT, desiredHeight)
     const y = scene.y2 - height
     super(x, y, width, height)
+
+    this.img = new Image(MOUNTAIN_WIDTH, MOUNTAIN_HEIGHT)
+    this.img.src = mountain
   }
 
   paint(context, speed) {
     this.x1 -= speed * 0.1
 
     context.beginPath()
-    context.fillStyle = 'yellow'
-    context.fillRect(this.x1, this.y1, this.width, this.height)
+    context.drawImage(this.img, this.x1, this.y1, this.width, this.height)
   }
 }
 
